0eeb31308fbd21a3c75dfbc95dde44e8673e671d
Rohit Kumar <38901817+29rohitkr@users.noreply.github.com>
2026-05-02 20:47:10 +0530

    basic middleware and express configuration

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+// Basic Middlewares
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 
 
 
