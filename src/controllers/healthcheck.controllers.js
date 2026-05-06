eed22817d59dd92150173771976f414018e1b8c4
Rohit Kumar <38901817+29rohitkr@users.noreply.github.com>
2026-05-06 08:51:35 +0530

    implemented asyncHandler to avoid try-catch in countollers. Updated healthCheck controller using asyncHandler

--- src/controllers/healthcheck.controllers.js
+++ src/controllers/healthcheck.controllers.js
@@ -1,11 +1,23 @@
 import { APIResponse } from "../utils/api-response.js";
+import { asyncHandler } from "../utils/async-handler.js";
 
-const healthCheck = (req, res) => {
+/*
+const healthCheck = async (req, res, next) => {
     try {
+        // await some async operations if needed
         res.status(200).json(
             new APIResponse(200, { message: "Server is running" })
         );
-    } catch (error) { }
+    } catch (error) { 
+        next(error);
+    }
 }
+*/
+
+// Using asyncHandler to avoid try-catch in every controller    
+
+const healthCheck = asyncHandler(async (req, res, next) => {
+    res.status(200).json(new APIResponse(200, { message: "Server is running." }));
+});
 
 export default healthCheck; // { healthCheck }
