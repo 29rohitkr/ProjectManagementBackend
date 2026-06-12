89dd437deea75d9193a5636c5be02abb200b8bc2
Rohit Kumar <38901817+29rohitkr@users.noreply.github.com>
2026-06-12 16:55:11 +0530

    task routes completed

---
+++ src/routes/task.routes.js
@@ -0,0 +1,73 @@
+import { Router } from "express";
+
+import {
+    getTasks,
+    getTaskById,
+    createTask,
+    updateTask,
+    deleteTask,
+    createSubTask,
+    updateSubTask,
+    deleteSubTask
+} from "../controllers/task.controllers.js"
+import {
+    createTaskValidator,
+    createSubTaskValidator
+} from "../validators/index.js";
+import { validate } from "../middleware/validator.middleware.js";
+import { verifyJWT, validateProjectPermission } from "../middleware/auth.middleware.js";
+import { AvailableUserRoles, UserRolesEnum } from "../utils/constants.js";
+
+const router = Router()
+router.use(verifyJWT);
+
+router
+    .route("/:projectId")
+    .get(
+        validateProjectPermission(AvailableUserRoles),
+        getTasks
+    )
+    .post(
+        validateProjectPermission([UserRolesEnum.ADMIN, UserRolesEnum.PROJECT_ADMIN]),
+        createTaskValidator(),
+        validate,
+        createTask
+    )
+
+
+router
+    .route("/:projectId/t/:taskId")
+    .get(
+        validateProjectPermission(AvailableUserRoles),
+        getTaskById
+    )
+    .put(
+        validateProjectPermission([UserRolesEnum.ADMIN, UserRolesEnum.PROJECT_ADMIN]),
+        createSubTaskValidator(),
+        validate,
+        updateTask
+    )
+    .delete(
+        validateProjectPermission([UserRolesEnum.ADMIN, UserRolesEnum.PROJECT_ADMIN]),
+        deleteTask
+    )
+router
+    .route("/:projectId/t/:taskId/subtasks")
+    .post(validateProjectPermission([UserRolesEnum.ADMIN, UserRolesEnum.PROJECT_ADMIN]),
+        createSubTaskValidator(),
+        validate,
+        createSubTask
+    )
+
+router
+    .route("/:projectId/st/:subTaskId")
+    .put(validateProjectPermission(AvailableUserRoles),
+        updateSubTask
+    )
+    .delete(
+        validateProjectPermission([UserRolesEnum.ADMIN, UserRolesEnum.PROJECT_ADMIN]),
+        deleteSubTask
+    )
+
+
+export default router;
